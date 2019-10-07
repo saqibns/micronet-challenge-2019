1fad9dae3e16b26e591de56ea1b060dfc2cce0e6
Saqib Shamsi <shamsi.saqib@gmail.com>
2019-10-07 13:45:18 +0530

    Added concat pooling branch during downsample

--- models/mobilenet_v2.py
+++ models/mobilenet_v2.py
@@ -3,6 +3,16 @@ from torch import nn
 from . import model_helpers
 
 
+class ConcatPool(nn.Module):
+    def __init__(self, kernel_size):
+        super().__init__()
+        self.apool = nn.AvgPool2d(kernel_size)
+        self.mpool = nn.MaxPool2d(kernel_size)
+
+    def forward(self, x):
+        return torch.cat([self.apool(x), self.mpool(x)], dim=1)
+
+
 class ResBlock(nn.Module):
     def __init__(self, inf, outf, expand, neg_slope, downsample=False):
         super().__init__()
@@ -42,8 +52,48 @@ class ResBlock(nn.Module):
         return x + y
 
 
+class ResBlock2(nn.Module):
+    def __init__(self, inf, outf, expand, neg_slope, downsample=False):
+        super().__init__()
+
+        # Expand Block
+        c = int(inf * expand)
+        self.downsample = downsample
+        if downsample:
+            mid_stride = 2
+        else:
+            mid_stride = 1
+        self.trunk = nn.Sequential(
+            nn.Conv2d(inf, c, 1, stride=1, padding=0),
+            nn.BatchNorm2d(c),
+            nn.LeakyReLU(negative_slope=neg_slope, inplace=True),
+
+            nn.Conv2d(c, c, 3, stride=mid_stride, padding=1, groups=c),
+            nn.BatchNorm2d(c),
+            nn.LeakyReLU(negative_slope=neg_slope, inplace=True),
+
+            nn.Conv2d(c, outf, 1, stride=1, padding=0),
+            nn.BatchNorm2d(outf)
+        )
+
+        # Initialize weights
+        model_helpers.weight_init(self.trunk)
+        # Initialize last batchnorm after the residual connnection (Xie et al)
+        if downsample:
+            self.pool = ConcatPool(2)
+        nn.init.zeros_(self.trunk[-1].weight)
+        nn.init.zeros_(self.trunk[-1].bias)
+
+    def forward(self, x):
+        y = self.trunk(x)
+        # Skip connection based on whether or not downsampling occurred
+        if self.downsample:
+            return self.pool(x) + y
+        return x + y
+
+
 class MobileNetV2(nn.Module):
-    def __init__(self, num_classes, alpha=1, dropout=0.3, neg_slope=0.2):
+    def __init__(self, num_classes, alpha=1, dropout=0.3, neg_slope=0.2, block=ResBlock):
         super().__init__()
 
         # Initial convolution for channel expansion
@@ -57,14 +107,14 @@ class MobileNetV2(nn.Module):
         nn.init.zeros_(self.init_bn.bias)
 
         self.features = nn.Sequential(
-                    ResBlock(32 * alpha, 32, alpha, neg_slope),
-                    ResBlock(32, 64, alpha, neg_slope, downsample=True),  # 16x16
-                    ResBlock(64, 64, alpha, neg_slope),
-                    ResBlock(64, 128, alpha, neg_slope, downsample=True),  # 8x8
-                    ResBlock(128, 128, alpha, neg_slope),
-                    ResBlock(128, 256, alpha, neg_slope, downsample=True),  # 4x4
-                    ResBlock(256, 256, alpha, neg_slope),
-                    ResBlock(256, 512, alpha, neg_slope, downsample=True)  # 2x2
+                    block(32 * alpha, 32, alpha, neg_slope),
+                    block(32, 64, alpha, neg_slope, downsample=True),  # 16x16
+                    block(64, 64, alpha, neg_slope),
+                    block(64, 128, alpha, neg_slope, downsample=True),  # 8x8
+                    block(128, 128, alpha, neg_slope),
+                    block(128, 256, alpha, neg_slope, downsample=True),  # 4x4
+                    block(256, 256, alpha, neg_slope),
+                    block(256, 512, alpha, neg_slope, downsample=True)  # 2x2
                 )
 
         self.pool = nn.AvgPool2d(2)
